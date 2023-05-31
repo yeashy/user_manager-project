b79a3d53d05e7d1c3e53a6bfbb3fd50d8d13e9f9
yeashy <gertyn07@gmail.com>
2023-05-31 10:14:19 +0700

    сделал главную страницу

---
+++ app/resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ru">
+@include('partials.common.head')
+<body>
+@include('partials.common.header')
+<div class="content_wrapper">
+    @yield('content')
+</div>
+</body>
+</html>
